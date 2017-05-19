f2f6fff99a55013417a826c0b430349166ed794b
Daniel-Ems <danielems3@outlook.com>
2017-05-19 11:38:38 -0400

    works up to 100

--- fibonacci.s
+++ fibonacci.s
@@ -3,7 +3,7 @@
 ErrorStr:
 	.asciz "Usage ./fibonacci <0-100>"
 Arg:
-	.asciz "number you passed %lu\n"
+	.asciz "number you passed Ox%lx%lx\n"
 .globl main
 main:
 	cmp rdi, 2
@@ -24,6 +24,8 @@ main:
 	cmove rdx, rax
 	je 3f
 
+	xor r11, r11
+	xor r14, r14
 	mov rcx, 1
 	mov rdx, 1
 	mov rbx, 1
@@ -33,12 +35,15 @@ main:
 2:
 	inc rcx
 	xadd rbx, rdx
+	adc r11, r14
+	xchg r14, r11
 	#xchg rbx, rdx
 	jmp 1b
 3:
 	sub rsp, 8
 	mov rdi, OFFSET Arg
-	mov rsi, rdx
+	mov rsi, r11
+	mov rdx, rdx
 	call printf
 	add rsp, 8 
 	ret
